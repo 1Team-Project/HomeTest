3c543b1b1a913c684070ec8e9a9ecdf72f697630
nahyunoh <82365879+nahyunoh@users.noreply.github.com>
2021-07-05 15:03:46 +0900

    Delete security_daoAuthenticate directory

--- security_daoAuthenticate/src/main/java/com/spring/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package com.spring.service;
-
-import com.spring.security.SpUser;
-import com.spring.security.SpUserAuthority;
-
-public interface MemberService {
-	public boolean register(SpUser user);
-	//public boolean registerAuth(SpUserAuthority auth);
-}
